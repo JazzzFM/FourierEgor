b7d5207b1346578fb7c75575b92b3bf9b3e5cf08
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2021-12-06 14:46:45 -0600

    Se añadieron algunos comentarios al código.

--- src/quad_spline.py
+++ src/quad_spline.py
@@ -15,32 +15,43 @@ from numpy.linalg import *
 # -coef_quad_spline returns the coefficients of our spline.
 # -'u' and 'v' are arrays withc together represents the points nedeed.
 def coef_quad_spline(u, v):
-    n = len(min(u,v))
-    m = n-1
-    A = zeros((2*m, 2*m))
-    b = zeros(2*m)
-
+    n = min(len(u),len(v))       # -In case of having len(u) != len(v).
+    m = n-1                      # -Number of coeficients to return.
+    A = zeros((2*m, 2*m))        # -Initializing our linear sistem with
+    b = zeros(2*m)               #  zeros.
+    
+    # -Creating our linear sistem.
+    # -Using the b_i(u_{i+1} - u_i) + c_i(u_{i+1} - u_i)^2 = v_{i+1} - v_i
+    #  equations.
     for i in range(m-1):
+        # -The A[i][i] numbers represent the b_i's coefficients.
         A[i][i] = u[i+1]-u[i]
+        # -The A[i][i+m] numbers represent the c_i's coefficients.
         A[i][i+m] = (u[i+1]-u[i])**2
         b[i] = v[i+1]-v[i]
     
+    # -Introducing the b_m(u_{m+1}-u_m) + c_m(u_{m+1}-u_m)^2 = v_1-v_m
+    #  equation.
     A[m-1][m-1] = u[m]-u[m-1]
     A[m-1][2*m-1] = (u[m]-u[m-1])**2
     b[m-1] = v[0]-v[m]
 
+    # -Still creating our linear sistem.
+    # -Using the b_i - b_{i+1} + 2c_i(u_{i+1}-u_i) = 0 equations.
     for i in range(m-1):
         A[i+m][i] = 1.0
         A[i+m][i+1] = -1.0
         A[i+m][i+m] = 2.0*(u[i+1]-u[i])
         
-        
+    # -Introducing the b_m -b_1 +2c_m(u_{m+1}-u_m) = 0 equation.    
     A[2*m-1][m-1] = 1.0
     A[2*m-1][0] = -1.0
     A[2*m-1][2*m-1] = 2.0*(u[m]-u[m-1])
     
+    #Debugging porpuses.
     print(A); print(b)
     
+    # -Solving our linear sistem.
     x = solve(A,b)
 
     return x
@@ -51,24 +62,4 @@ v = list([-1.0, 4.0, -1.0])
 
 x = coef_quad_spline(u, v)
 print(x)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                
+         
